8cd9ca629da80e20978fd06d03d83aff3ecb2b30
unknown <youssefoualifessuoy@gmail.com>
2021-12-15 13:01:43 +0100

    test setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import DashBord from "./Pages/DashBord";
+
 function App() {
-  return <div className="App">Hellow World!</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <DashBord />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/DashBord/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function DashBord() {
+  return (
+    <div className="box">
+      <div className="SideBar"></div>
+      <div className="DashBord">
+        <div className="Header"></div>
+        <div className="mainBord"></div>
+      </div>
+    </div>
+  );
+}
+
+export default DashBord;
