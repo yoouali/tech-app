d4a652cac3ac7c3086990057a7154068cd077923
unknown <youssefoualifessuoy@gmail.com>
2021-12-19 14:49:39 +0100

    dashboard dimensions

---
+++ src/Components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../Styling/index.css";
+
+function Header() {
+  return (
+    <div className="Header">
+      <div id="sideBarIcon" className="sideBarIcon"></div>
+    </div>
+  );
+}
+
+export default Header;
--- src/Pages/DashBord/index.js
+++ src/Pages/DashBord/index.js
@@ -1,12 +1,17 @@
 import React from "react";
+import "../../Styling/index.css";
+import Header from "../../Components/Header";
 
 function DashBord() {
   return (
     <div className="box">
-      <div className="SideBar"></div>
+      <Header />
       <div className="DashBord">
-        <div className="Header"></div>
-        <div className="mainBord"></div>
+        <div id="sideBar" className="SideBar"></div>
+        <div className="mainBord">
+          <div className="userBord"></div>
+          <div className="userDepa"></div>
+        </div>
       </div>
     </div>
   );
