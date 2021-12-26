23bca84ec45e460309a8eb384a5fb10a74cc7309
unknown <youssefoualifessuoy@gmail.com>
2021-12-26 14:42:30 +0100

    get user

--- src/Pages/DashBord/index.js
+++ src/Pages/DashBord/index.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../Styling/index.css";
 import Header from "../../Components/Header";
 import API from "../../api.js";
+import { Redirect } from "react-router-dom";
 
 function DashBord() {
+  const [isLoading, setisLoading] = useState(true);
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const Token = localStorage.getItem("token");
+    if (!Token) return <Redirect to="/Login" />;
+    API.get("user")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
   return (
     <div className="box">
       <Header />
